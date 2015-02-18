260201217e199a0a6f8b2db842c984c806b4a125
pal <pal@zuehlke.com>
2015-02-18 07:24:00 +0100

    Added missing namespace to unit test and made task show expected behavior of having latency.

--- ReactiveHUB.Core.Tests/UnitTest1.cs
+++ ReactiveHUB.Core.Tests/UnitTest1.cs
@@ -1,60 +1,70 @@
-﻿using System.Reactive;
-using System.Reactive.Linq;
-using System.Reactive.Subjects;
-using System.Reactive.Threading.Tasks;
-using System.Threading.Tasks;
-using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
-
-[TestClass]
-public class Test
+﻿namespace ReactiveHUB.Core.Tests
 {
-    private Mock<IRemoteWebAPI> remoteWebAPI;
+    using System.Reactive.Linq;
+    using System.Reactive.Threading.Tasks;
+    using System.Threading.Tasks;
+    using System;
 
-    [TestInitialize]
-    public void testInit()
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Moq;
+
+    [TestClass]
+    public class Test
     {
-        // The api returns a few entries when GetLatestValues is called and then continues with some values in the stream
-        remoteWebAPI = new Mock<IRemoteWebAPI>();
-        remoteWebAPI.Setup(x => x.GetLatestValues()).Returns(Task.FromResult(new[] { 1, 2, 3, 4, 5 }));
-        remoteWebAPI.Setup(x => x.SubscribeToNewValues()).Returns(new[] { 6, 7, 8, 9, 10 }.ToObservable());
-    }  
+        private Mock<IRemoteWebAPI> remoteWebAPI;
 
-    [TestMethod]
-    public async Task TestMethod()
-    {
-
-        var myObservable = this.MakeMyObservable(remoteWebAPI.Object);
-
-        var items = await myObservable.ToArray().FirstAsync();
-
-        // I want first all the values returned from the async call, followed by everything that the stream returns
-        CollectionAssert.AreEqual(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, items);
-    }
-
-    public IObservable<int> MakeMyObservable(IRemoteWebAPI myApi)
-    {
-        IObservable<int> result = new int[] {}.ToObservable();
-        var latest = myApi.GetLatestValues().ToObservable();
-        var newer = myApi.SubscribeToNewValues();
-
-        var hotSource = Observable.Publish(latest);
-        
-        hotSource.Subscribe(ints =>
+        [TestInitialize]
+        public void testInit()
         {
-            var tmp = ints.ToObservable();
-            result = tmp.Concat(newer);
-        });
-        hotSource.Connect();
+            // The api returns a few entries when GetLatestValues is called and then continues with some values in the stream
+            remoteWebAPI = new Mock<IRemoteWebAPI>();
+            remoteWebAPI.Setup(x => x.SubscribeToNewValues()).Returns(new[] { 6, 7, 8, 9, 10 }.ToObservable());
+            remoteWebAPI.Setup(x => x.GetLatestValues()).Returns(this.GetInts);
+        }
 
-        return result;
+        [TestMethod]
+        public async Task TestMethod()
+        {
+
+            var myObservable = this.MakeMyObservable(remoteWebAPI.Object);
+
+            var items = await myObservable.ToArray().FirstAsync();
+
+            // I want first all the values returned from the async call, followed by everything that the stream returns
+            CollectionAssert.AreEqual(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, items);
+        }
+
+        public IObservable<int> MakeMyObservable(IRemoteWebAPI myApi)
+        {
+            IObservable<int> result = new int[] { }.ToObservable();
+            var latest = myApi.GetLatestValues().ToObservable();
+            var newer = myApi.SubscribeToNewValues();
+
+            var hotSource = Observable.Publish(latest);
+
+            hotSource.Subscribe(
+                ints =>
+                    {
+                        var tmp = ints.ToObservable();
+                        result = tmp.Concat(newer);
+                    });
+            hotSource.Connect();
+
+            return result;
+        }
+
+        public async Task<int[]> GetInts()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            return new[] { 1, 2, 3, 4, 5 };
+        }
     }
-}
 
-public interface IRemoteWebAPI
-{
-    Task<int[]> GetLatestValues();
+    public interface IRemoteWebAPI
+    {
+        Task<int[]> GetLatestValues();
 
-    IObservable<int> SubscribeToNewValues();
+        IObservable<int> SubscribeToNewValues();
+    }
 }
