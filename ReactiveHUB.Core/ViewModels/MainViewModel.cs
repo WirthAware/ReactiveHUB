ec5df030ff1a81a60bda942e20fd538d20a09a18
pal <pal@zuehlke.com>
2015-03-19 15:20:23 +0100

    Started work on routing

---
+++ ReactiveHUB.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿// // --------------------------------------------------------------------------------------------------------------------
+// // <copyright file="MainViewModel.cs" company="Zühlke Engineering GmbH">
+// //   Zühlke Engineering GmbH
+// // </copyright>
+// // <summary>
+// //   MainViewModel.cs
+// // </summary>
+// // --------------------------------------------------------------------------------------------------------------------
+namespace ProjectTemplate.ViewModels
+{
+    using ReactiveUI;
+
+    public class MainViewModel : ReactiveObject, IScreen
+    {
+        protected MainViewModel(IRoutingState router)
+        {
+            this.Router = router ?? new RoutingState();
+
+            this.Router.Navigate.Execute(new MessagesViewModel());
+        }
+
+        public IRoutingState Router { get; private set; }
+    }
+}
---
+++ ReactiveHUB.Core/ViewModels/SomeDialogViewModel.cs
@@ -0,0 +1,24 @@
+﻿// // --------------------------------------------------------------------------------------------------------------------
+// // <copyright file="SomeDialogViewModel.cs" company="Zühlke Engineering GmbH">
+// //   Zühlke Engineering GmbH
+// // </copyright>
+// // <summary>
+// //   SomeDialogViewModel.cs
+// // </summary>
+// // --------------------------------------------------------------------------------------------------------------------
+namespace ProjectTemplate.ViewModels
+{
+    using System.Windows.Input;
+
+    using ReactiveUI;
+
+    public class SomeDialogViewModel
+    {
+        public SomeDialogViewModel(IScreen host)
+        {
+            this.CloseCommand = host.Router.NavigateBack;
+        }
+
+        public ICommand CloseCommand { get; private set; }
+    }
+}
